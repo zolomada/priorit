8d17e660adfc3334256a73f8370e42c650b2d17a
Zainab Olomada <znbolomada@gmail.com>
2024-03-26 13:00:30 -0400

    installed knex, linked db to server and created user table

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
