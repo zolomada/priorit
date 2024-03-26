1ab004bb6d566f2b5bcdb5704736add3c7969151
Zainab Olomada <znbolomada@gmail.com>
2024-03-26 16:28:24 -0400

    sign up form created

--- client/src/Pages/SignUpPage/SignUpPage.js
+++ client/src/Pages/SignUpPage/SignUpPage.js
@@ -1,7 +1,18 @@
 import "./SignUpPage.scss";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import { Link } from "react-router-dom";
 
 function SignUpPage() {
-  return <div></div>;
+  return (
+    <div>
+      <SignUpForm />
+      <div>
+        <p>
+          Have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpPage;
--- client/src/components/SignUpForm/SignUpForm.js
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -1,7 +1,73 @@
 import "./SignUpForm.scss";
+import { useState } from "react";
+import axios from "axios";
+import { baseUrl } from "../../global";
 
 function SignUpForm() {
-  return <div></div>;
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(`${baseUrl}sign-up`, values);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form className="signup" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Full Name</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="name"
+            className="signup__name"
+            onChange={(e) => setValues({ ...values, name: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="name"
+            className="signup__name"
+            onChange={(e) => setValues({ ...values, email: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">Password</label>
+          <input
+            type="password"
+            placeholder="Enter a password"
+            name="password"
+            className="signup__password"
+            onChange={(e) => setValues({ ...values, password: e.target.value })}
+          />
+        </div>
+        {/* <div>
+          <label htmlFor="name">Confirm Password</label>
+          <input
+            type="password"
+            placeholder="Enter a password"
+            name="password"
+            className="signup__password"
+          />
+        </div> */}
+        <button type="button" className="signup__button">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUpForm;
